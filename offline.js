5d7e71d435b8027a1b26f1daac882f39a30c098d
tppr2046 <gopmaster@hotmail.com>
2019-08-08 13:16:52 +0800

    test

--- offline.js
+++ offline.js
@@ -1,12 +1,11 @@
 ﻿{
-	"version": 1565237784,
+	"version": 1565241357,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
 		"jquery-2.1.1.min.js",
 		"offlineClient.js",
-		"images/story_pic-sheet0.png",
-		"images/option_card-sheet0.png",
+		"images/comic-sheet0.png",
 		"icon-16.png",
 		"icon-32.png",
 		"icon-114.png",
