f616d424925a367e7ac92da1f5e9b0eb1e0bc23e
tppr2046 <gopmaster@hotmail.com>
2019-10-27 22:43:26 +0800

    step1測試

--- offline.js
+++ offline.js
@@ -1,16 +1,23 @@
 ﻿{
-	"version": 1568206509,
+	"version": 1572187374,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
 		"jquery-2.1.1.min.js",
 		"offlineClient.js",
-		"images/sprite-sheet0.png",
+		"images/cover-sheet0.png",
+		"images/story_image-sheet0.png",
+		"images/story_image-sheet1.png",
+		"images/story_image-sheet2.png",
+		"images/story_image-sheet3.png",
+		"images/text_frame-sheet0.png",
+		"images/button1-sheet0.png",
 		"icon-16.png",
 		"icon-32.png",
 		"icon-114.png",
 		"icon-128.png",
 		"icon-256.png",
-		"loading-logo.png"
+		"loading-logo.png",
+		"quest.xml"
 	]
 }
